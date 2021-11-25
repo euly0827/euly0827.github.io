339c930542fcb14cff9e96ade3c697a08422262b
Euly <598379204@qq.com>
2021-11-26 01:12:51 +0800

    save

--- logic.js
+++ logic.js
@@ -1,56 +1,66 @@
 
 function show(){
     $(".cover").hide();
+    var isex = rd(2); 
     $.getJSON("./rdata.json", function (data){
-        var $jsontip = $("#jsonTip");
         var strHtml = "";
-        //存储数据的变量
-        $jsontip.empty();
-        //清空内容
-        console.log(data);
+        // console.log(data);
+        console.log(getJsonLength(data,"scar"));
+        console.log(isex);
+        console.log(data.sex[isex]);
         
-        $.each(data, function (infoIndex, info){
-            console.log(infoIndex+ info);
-            var i = 0;
-            var one = info;
-            console.log(one);
-            $.each(one, function (infoIndex, info){
-                console.log(infoIndex+ info);
-                strHtml += one[i].name;
-                i++;
-            }) 
-            document.getElementById("sex").innerHTML=strHtml; // 改变内容
-            document.getElementById("result").style.display="block";
-        }) 
+
       })
-    // var strsex = "性别：" + "男";
-   
+      document.getElementById("result").style.display="block";
 }
 
+function rd(length){
+    return parseInt(Math.random()*length,10);
+}
 
-$.getJSON("./rdata.json", function (data){
-    var $jsontip = $("#jsonTip");
-    var strHtml = "";
-    //存储数据的变量
-    $jsontip.empty();
-    //清空内容
-    console.log(data);
+function getHsonLength(json){
+    var jsonLength=0;
+    for (var i in json) {
+        jsonLength++;
+    }
+    return jsonLength;
+}
+
+function getJsonLength(jsonData,name){
+    　　var jsonLength = 0;
+    　　for(var item in jsonData){
+    　　　　if(item == name){
+    　　　　　　for(var x in jsonData[item]){
+    　　　　　　　　jsonLength++;
+    　　　　　　}
+    　　　　}
+    　　}
+            return jsonLength;
+    }
+
+// $.getJSON("./rdata.json", function (data){
+//     var $jsontip = $("#jsonTip");
+//     var strHtml = "";
+//     //存储数据的变量
+//     $jsontip.empty();
+//     //清空内容
+//     console.log(data);
     
-    $.each(data, function (infoIndex, info){
-        console.log(infoIndex+ info);
-        var i = 0;
-        var one = info;
-        console.log(one);
-        $.each(one, function (infoIndex, info){
-            console.log(infoIndex+ info);
-            strHtml += "姓名：" + one[i].name + "<br>";
-            strHtml += "性别：" + one[i].sex + "<br>";
-            strHtml += "邮箱：" + one[i].email + "<br>";
-            strHtml += "<hr>" ;
-            i++;
-        }) 
+//     $.each(data, function (infoIndex, info){
+//         console.log(infoIndex+ info);
+//         var i = 0;
+//         var one = info;
+//         console.log(one);
+//         $.each(one, function (infoIndex, info){
+//             console.log(infoIndex+ info);
+//             strHtml += "姓名：" + one[i].name + "<br>";
+//             strHtml += "性别：" + one[i].sex + "<br>";
+//             strHtml += "邮箱：" + one[i].email + "<br>";
+//             strHtml += "<hr>" ;
+//             i++;
+//         }) 
      
-    }) 
-    $jsontip.html(strHtml);
-    //显示处理后的数据 
-  })
+//     }) 
+//     $jsontip.html(strHtml);
+//     //显示处理后的数据 
+//   })

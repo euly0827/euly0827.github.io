29e3176178c174521b3a30557591bbf4f257c60a
Euly <598379204@qq.com>
2021-12-05 02:05:16 +0800

    优化了职业选择的逻辑，增加了年龄限制的判断

--- logic.js
+++ logic.js
@@ -1,13 +1,19 @@
 var time = 0;
 var call = 0;
+
+var iage,isex;
+
 function show() {
     $(".cover").hide();
     $.getJSON("./rdata.json", function (data) {
-        var isex = rd(getJsonLength(data, "sex"));
+        isex = rd(getJsonLength(data, "sex"));
+        iage = Math.floor(Math.random() * 30 + 15);
+
+        
         //性别
         document.getElementById("sex").innerHTML = data.sex[isex];
         //年龄
-        document.getElementById("age").innerHTML = Math.floor(Math.random() * 30 + 15);
+        document.getElementById("age").innerHTML = iage;
         //性格
         document.getElementById("character").innerHTML = data.character[rd(getJsonLength(data, "character"))];
         //发型与着装，男女区分
@@ -22,9 +28,7 @@ function show() {
         if (time == 0) {
             //年代显示
             document.getElementById("time").innerHTML = "1920s";
-            //职业
-            document.getElementById("job").innerHTML = data.beforejob[rd(getJsonLength(data, "beforejob"))].name + "  (序号" + data.beforejob[rd(getJsonLength(data, "beforejob"))].num + ")";
-            //重要之人
+           //重要之人
             document.getElementById("someone").innerHTML = data.someone[rd(getJsonLength(data, "someone"))];
             //重要之地
             document.getElementById("somewhere").innerHTML = data.beforesomewhere[rd(getJsonLength(data, "beforesomewhere"))];
@@ -48,11 +52,9 @@ function show() {
             var Ar = rd3(getJsonLength(data, "beforebelongings"));
             document.getElementById("belongings").innerHTML = data.beforebelongings[Ar[0]] + "、" + data.beforebelongings[Ar[1]] + "、" + data.beforebelongings[Ar[2]];
 
-        }else if (time == 1) {//现代
+        } else if (time == 1) {//现代
             //年代显示
             document.getElementById("time").innerHTML = "现代";
-            //职业
-            document.getElementById("job").innerHTML = data.nowjob[rd(getJsonLength(data, "nowjob"))].name + "  (序号" + data.nowjob[rd(getJsonLength(data, "nowjob"))].num + ")";
             //重要之人
             document.getElementById("someone").innerHTML = data.someone[rd(getJsonLength(data, "someone"))];
             //重要之地
@@ -76,8 +78,10 @@ function show() {
             //随身物品
             var Ar = rd3(getJsonLength(data, "nowbelongings"));
             document.getElementById("belongings").innerHTML = data.nowbelongings[Ar[0]] + "、" + data.nowbelongings[Ar[1]] + "、" + data.nowbelongings[Ar[2]];
-
+            
         }
+        //职业
+        foundjob(data);
     })
     document.getElementById("result").style.display = "block";
     //   console.log(rd(getJsonLength(data,"sex")));
@@ -142,20 +146,54 @@ function now() {
     document.getElementById("btnbefore2").style.backgroundColor = "rgb(239,199,191)";
 }
 
-function callme(){
+function callme() {
     call++;
     console.log(1);
-    switch(call){
+    switch (call) {
         case 1:
             alert("作者摸鱼中，联系是不可能联系得上的Ψ(￣∀￣)Ψ");
             break;
         case 2:
             alert("其实吧，想找作者也不是不行...");
-        break;
+            break;
         case 3:
             alert("但是你得先保证你找到了不会催更ヽ(#`Д´)ﾉ！");
-        break;
+            break;
         default:
             alert("ヽ(￣▽￣)ﾉ那好叭，你可以加一下这个群：540184048");
     }
+}
+
+function foundjob(data) {
+    if (iage <= 18) {
+        if (Math.random() < 0.7) {
+            document.getElementById("job").innerHTML = "学生/实习生 (序号106)";
+            console.log("1");
+        } else {
+            var arr = [1, 15, 22, 31, 38, 42, 45, 52, 61, 62, 73, 94, 106, 111, 117, 118, 119, 120, 152, 215, 216, 227];
+            var key = arr[rd(arr.length)];
+            for (var i in arr) {
+                if(time==0){
+                    for (var item in data.beforejob) {
+                        if (data.beforejob[item].num == key) {
+                            var jValue = data.beforejob[item].name;
+                            console.log(jValue);
+                            document.getElementById("job").innerHTML = jValue + " (序号" + key + ")";
+                        }
+                    }
+                }else{
+                    for (var item in data.nowjob) {
+                        if (data.beforejob[item].num == key) {
+                            var jValue = data.nowjob[item].name;
+                            console.log(jValue);
+                            document.getElementById("job").innerHTML = jValue + " (序号" + key + ")";
+                        }
+                    }
+                }
+                
+            }
+
+        }
+    }
+
 }
